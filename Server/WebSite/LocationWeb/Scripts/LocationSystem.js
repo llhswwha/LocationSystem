8fb89a629fee41df35693d99578d130168b00f43
caiwanwei <llhswwha@gmail.com>
2018-12-23 10:21:51 +0800

    家里更新

--- Server/WebSite/LocationWeb/Scripts/LocationSystem.js
+++ Server/WebSite/LocationWeb/Scripts/LocationSystem.js
@@ -1,14 +1,41 @@
 $(function () {
     $("#exe[href]").click(function (event) {
+
+        //console.log("LocationSystem.js");
+        window.protocolCheck($(this).attr("href"),
+            function () {
+                var downloadEXE = window.confirm("exe程序未安装,是否下载安装包!")
+                if (downloadEXE) {
+                    window.location.href = "/Exe/LocationSystem.exe";
+
+                    //$.ajax({
+                    //    type: "get",
+                    //    url: "Arg/GetDownloadLink?relativePath=/Exe/LocationSystem.exe",                   
+                    //    contentType: "text/html; charset=utf-8",
+                    //    //dataType: "json",
+                    //    async: false,
+                    //    success: function (data) {
+                    //        console.log(data);
+                    //        window.location.href = data;                            
+                    //    },
+                    //    error: function (XMLHttpRequest, textStatus, errorThrown) {                           
+                    //        alert("失败！");
+                    //    }
+                    //});
+                } 
+                //alert("exe程序未安装,请下载!");                               
+            });
+        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+
         $.ajax({
             type: "get",
             url: "Arg/GetLoginInfo",
-            //data: 'locations', //data 表示发送到服务端的数据，键值对或字符串
+            //data: 'locations',
             contentType: "text/html; charset=utf-8",
             //dataType: "json",
             async: false,
             success: function (data) {
-                //console.log(data);
+                //console.log(data);               
                 $(function () {
                     $("#exe").attr("href", "LocationSystem:");
                     //当前href
@@ -27,24 +54,7 @@ $(function () {
                 //console.log(textStatus);
                 alert("失败！");
             }
-        });
-       
-        //console.log("LocationSystem.js");
-        window.protocolCheck($(this).attr("href"),
-            function () {
-                var downloadEXE = window.confirm("exe程序未安装,是否下载安装包!")
-                if (downloadEXE) {
-                    //window.alert("下载");
-                    window.open("http://localhost:53506/../Exe/LocationSystem.exe");           
-                } else {
-                    //window.alert("取消");
-                    return false;
-                }
-                    
-                //alert("exe程序未安装,请下载!");                
-                //window.open("http://localhost:53506/../Exe/LocationSystem.exe");
-            });
-        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+        });        
     });            
 });
 
