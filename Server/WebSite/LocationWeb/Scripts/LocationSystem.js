ae05ffdf3e6b333664b4e8e5974924dc2a5899d7
caiwanwei <llhswwha@gmail.com>
2018-12-25 10:56:28 +0800

    家里提交

--- Server/WebSite/LocationWeb/Scripts/LocationSystem.js
+++ Server/WebSite/LocationWeb/Scripts/LocationSystem.js
@@ -1,7 +1,38 @@
+window.onload = function () {
+    $.ajax({
+        type: "get",
+        url: "Arg/GetLoginInfo",
+        //data: 'locations',
+        contentType: "text/html; charset=utf-8",
+        //dataType: "json",
+        async: false,
+        success: function (data) {
+            console.log(data);
+            $(function () {
+                $("#exe").attr("href", "LocationSystem:");
+                //当前href
+                var location = $("#exe").attr("href");
+                //console.log(location);
+                //连接字段
+                var newStr = location.concat(data);
+                //console.log(newStr);
+                $("#exe").attr("href", newStr);
+            });
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            //console.log("error");
+            //console.log(XMLHttpRequest.status);
+            //console.log(XMLHttpRequest.readyState);
+            //console.log(textStatus);
+            alert("失败！");
+        }
+    });
+}
+
 $(function () {
     $("#exe[href]").click(function (event) {
 
-        //console.log("LocationSystem.js");
+        console.log("LocationSystem.js");
         window.protocolCheck($(this).attr("href"),
             function () {
                 var downloadEXE = window.confirm("exe程序未安装,是否下载安装包!")
@@ -25,36 +56,7 @@ $(function () {
                 } 
                 //alert("exe程序未安装,请下载!");                               
             });
-        event.preventDefault ? event.preventDefault() : event.returnValue = false;
-
-        $.ajax({
-            type: "get",
-            url: "Arg/GetLoginInfo",
-            //data: 'locations',
-            contentType: "text/html; charset=utf-8",
-            //dataType: "json",
-            async: false,
-            success: function (data) {
-                //console.log(data);               
-                $(function () {
-                    $("#exe").attr("href", "LocationSystem:");
-                    //当前href
-                    var location = $("#exe").attr("href");
-                    //console.log(location);
-                    //连接字段
-                    var newStr = location.concat(data);
-                    //console.log(newStr);
-                    $("#exe").attr("href", newStr);
-                });
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                //console.log("error");
-                //console.log(XMLHttpRequest.status);
-                //console.log(XMLHttpRequest.readyState);
-                //console.log(textStatus);
-                alert("失败！");
-            }
-        });        
+        event.preventDefault ? event.preventDefault() : event.returnValue = false;        
     });            
 });
 
